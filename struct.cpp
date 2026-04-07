ac13f0e77126202681808d723d8f954354994d1b
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-07 18:29:36 +0700

    commit 10

--- struct.cpp
+++ struct.cpp
@@ -14,5 +14,19 @@ int main()
     //deklarasi variabel struct
     Orang mhs;
     //mengisi data
-    cout << "Isikan data nama : "
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data Tampil" << endl;
+    cout << "Nama : "  << mhs.nama  << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "umur : " << mhs.umur << endl;
+    
+
+
 }
