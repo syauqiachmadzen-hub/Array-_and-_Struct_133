83346e8f8049be97078417330f51998f266e6918
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-07 17:18:15 +0700

    commit 2

--- array.cpp
+++ array.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    //Deklarasi array disertai inisialisasi
+    int dataku [5] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    int Datamu[5]  
+    
+}
