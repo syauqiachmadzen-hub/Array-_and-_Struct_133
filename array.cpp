c97e582baea83ccb255b2b21255e3e7d821ea301
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-07 17:23:51 +0700

    commit 4

--- array.cpp
+++ array.cpp
@@ -11,6 +11,8 @@ int main()
     //Cara akses (membaca atau mengambil isi array)
     cout << "isi data kedua : " << dataku[1] << endl;
     //mengisi array
-    dataku[1 = 200;]
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
+
     
 }
