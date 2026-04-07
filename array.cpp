babd2533670540c8a5f458c99297e489f5edcbbf
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-07 17:37:13 +0700

    commit 6

--- array.cpp
+++ array.cpp
@@ -25,4 +25,13 @@ int main()
     cin >> datamu[3];
     cout << "Isikan data kelima : ";
     cin >> datamu[4];
+    cout << endl;
+
+    //menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << " data ketiga  = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+    cout << endl;
 }
