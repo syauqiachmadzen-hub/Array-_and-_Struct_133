4d8838751346adb33f06ac737d0c6805acceeea9
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-07 18:22:25 +0700

    commit 9

---
+++ struct.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+
+};
+
+int main()
+{
+    //deklarasi variabel struct
+    Orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : "
+}
